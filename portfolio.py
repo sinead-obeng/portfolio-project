ddb79ae2ea5669fda5b18a529d5ef7fe48013aae
sinead-obeng <sinead.obeng@gmail.com>
2024-07-01 22:18:48 +0100

    Creating APIs for contact and about page

--- portfolio.py
+++ portfolio.py
@@ -10,5 +10,17 @@ def index():
     return render_template('index.html')
 
 
+# API for the about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# API for the contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0",port=5000)
