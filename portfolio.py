08b0d6345c25c6f7fd830b1f72907e33f636df0e
sinead-obeng <sinead.obeng@gmail.com>
2024-07-01 22:09:37 +0100

    Update to index.html and base.html

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 # API for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host="0.0.0.0",port=5000)
